8f17c15ebd75b90cd0ec1b55a292138c14c52b6c
Aryan Saxena <arysaxen@cisco.com>
2024-10-02 22:35:39 +0530

    Add per RPC

--- client/client.go
+++ client/client.go
@@ -1,31 +1,11 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package client is a containerz grpc client.
 package client
 
 import (
 	"context"
-	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
-	cpb "github.com/openconfig/gnoi/containerz"
-)
 
-var (
-	// Dial is the dailer to use to read containerz
-	Dial = grpc.DialContext
+	"google.golang.org/grpc"
+
+	cpb "github.com/openconfig/gnoi/containerz"
 )
 
 // Client is a grpc containerz client.
@@ -33,21 +13,30 @@ type Client struct {
 	cli cpb.ContainerzClient
 }
 
-// NewClient builds a new containerz client with username and password for authentication.
-func NewClient(ctx context.Context, addr, username, password string) (*Client, error) {
-	// Create metadata with username and password
-	md := metadata.Pairs(
-		"username", username,
-		"password", password,
-	)
+// creds holds the username and password for basic authentication.
+type creds struct {
+	Username string
+	Password string
+}
 
-	// Attach metadata to context
-	ctx = metadata.NewOutgoingContext(ctx, md)
+// GetRequestMetadata is needed by credentials.PerRPCCredentials.
+func (c *creds) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"username": c.Username,
+		"password": c.Password,
+	}, nil
+}
 
-	// Use grpc.WithInsecure() to avoid using TLS
-	conn, err := Dial(ctx, addr, grpc.WithInsecure())
+// RequireTransportSecurity is needed by credentials.PerRPCCredentials.
+func (c *creds) RequireTransportSecurity() bool {
+	return false
+}
+
+// NewClient builds a new containerz client.
+func NewClient(ctx context.Context, addr string) (*Client, error) {
+	conn, err := grpc.NewClient(addr, grpc.WithPerRPCCredentials(&creds{"cisco", "cisco123"}))
 	if err != nil {
-		return nil, fmt.Errorf("failed to dial server: %v", err)
+		return nil, err
 	}
 
 	return &Client{
